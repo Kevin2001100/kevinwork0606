73673c9d11d806b382672b509558f1ba424a4a1c
Kevin <Kevingotop100@gmail.com>
2025-06-05 17:39:06 +0800

    更新產品圖片及資訊，新增特價商品，改善產品展示

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 // 產品資料
 const products = [
     {
-        img: "img/cup1.png",
+        img: "img/p1.webp",
         tag: "特價",
         title: "【貓咪柴犬2杯2袋組】速吸杯二代",
         desc: "900ml+720ml+飲料提袋包覆款和輕巧款",
@@ -9,23 +9,49 @@ const products = [
         price: "NT$1,799"
     },
     {
-        img: "img/cup2.png",
+        img: "img/p2.webp",
         tag: "熱銷",
-        title: "聯名環保杯組",
-        desc: "插畫家聯名設計，限量發售",
+        title: "【速吸杯二代】",
+        desc: "1200ml (內膽陶瓷層) - 柴犬/貓咪",
         priceOld: "NT$2,000",
         price: "NT$1,500"
     },
-    { img: "img/p1.webp", title: "商品3", price: "NT$1,460" },
-    { img: "img/p2.webp", title: "商品4", price: "NT$1,200" },
-    { img: "img/p3.webp", title: "商品5", price: "NT$1,180" },
-    { img: "img/p4.webp", title: "商品6", price: "NT$1,160" },
-    { img: "img/p5.webp", title: "商品7", price: "NT$1,140" },
-    { img: "img/p6.webp", title: "商品8", price: "NT$1,120" },
-    { img: "img/p6.webp", title: "商品9", price: "NT$1,100" },
-    { img: "img/p8.webp", title: "商品10", price: "NT$1,080" },
-    { img: "img/p9.webp", title: "商品11", price: "NT$1,060" },
-    { img: "img/p10.webp", title: "商品12", price: "NT$1,040" }
+    {
+        img: "img/p3.webp",
+        tag: "特價",
+        title: "【媽祖1杯1袋組】蓋賀杯",
+        desc: "700ml+飲料提袋高大款",
+        priceOld: "NT$1,500",
+        price: "NT$799"
+    },
+     {
+        img: "img/p4.webp",
+        tag: "特價",
+        title: "【速吸杯二代】+【蓋賀杯】",
+        desc: "900ml+700ml+飲料提袋包覆款和高大款",
+        priceOld: "NT$3,100",
+        price: "NT$1,499"
+    },
+     {
+        img: "img/p5.webp",
+        tag: "特價",
+        title: "【速吸杯二代】",
+        desc: "1200ml (內膽陶瓷層) - 龍",
+        priceOld: "NT$1,500",
+        price: "NT$1,380"
+    },
+
+     {
+        img: "img/p6.webp",
+        tag: "特價",
+        title: "【蓋賀杯】",
+        desc: "700ml - 微醺粉",
+        priceOld: "NT$1,200",
+        price: "NT$1,180"
+    },
+    
+    
+    
 ];
 
 // 動態產生產品卡片
